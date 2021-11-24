10e70ba2a8ee9326334f68c5ad0cf564427ed5b1
MichaelCullenMadeTech <79979174+MichaelCullenMadeTech@users.noreply.github.com>
2021-11-24 09:16:41 +0000

    CRL and OCSP test (#17)
    CRL test was quite simple, set up a CRL, and then revoke a certificate, then redo the CRL. This CRL file is updated from index.txt and then displays the revocation state of the certificate in questio.
    OCSP test was a little more difficult, if you want to set up your own OCSP certificate (still a pem file) you have to create an OCSP cert, as an intermediate certificate, which signs other certificates. Because we do not have the ability to change much within the structure of freeradius we just have to append the details onto the ca config, before the make is run against the freeradius creation of certificates.
    This then is overwritten at run-time anyway, and allows us to check. In production, these values are set to "no overwritten OCSP endpoints", which means it will use the correct format of certificates if available.

--- test/generate_certs/bootstrap.sh
+++ test/generate_certs/bootstrap.sh
@@ -4,20 +4,51 @@ set -e
 
 prefix=/etc/freeradius/3.0
 
+append_ocsp_endpoints() {
+  echo "authorityInfoAccess = OCSP;URI:http://www.example.com:8080" >> ca.cnf
+}
+
 generate_certs() {
   make
 }
 
 generate_revoked_client_certs() {
-  if ! [ -f $prefix/certs/revoked_client.pem ]; then
-    openssl req -batch -passin pass:"whatever" -config client_crl.cnf -new -nodes -keyout client_crl.key -out client_crl.csr
-    openssl ca  -batch -passin pass:"whatever" -config ca.cnf -in client_crl.csr -out revoked_client.pem -keyfile ca.key -cert ca.pem
+  # generate crl pem file
+  openssl ca -batch -gencrl -keyfile ca.key -cert ca.pem -config ca.cnf -passin pass:"whatever" -out crl.pem
 
-    openssl ca -batch -revoke revoked_client.pem -keyfile ca.key -cert ca.pem -config ca.cnf -passin pass:"whatever"
+  # create a revoked crl certificate
+  if ! [ -f $prefix/certs/revoked_client_crl.pem ]; then
+    openssl req -batch -passin pass:"whatever" -config client_crl.cnf -new -nodes -keyout client_crl.key -out client_crl.csr
+    openssl ca  -batch -passin pass:"whatever" -config ca.cnf -in client_crl.csr -out revoked_client_crl.pem -keyfile ca.key -cert ca.pem
+
+    cat client_crl.key >> revoked_client_crl.pem
+
+    openssl ca -batch -revoke revoked_client_crl.pem -keyfile ca.key -cert ca.pem -config ca.cnf -passin pass:"whatever"
+
+    # regenerate crl pem file
+    openssl ca -batch -gencrl -keyfile ca.key -cert ca.pem -config ca.cnf -passin pass:"whatever" -out crl.pem
+  fi
+
+  # create a revoked ocsp certificate
+  if ! [ -f $prefix/certs/revoked_client_ocsp.pem ]; then
+    openssl req -batch -passin pass:"whatever" -config client_ocsp.cnf -new -nodes -keyout client_ocsp.key -out client_ocsp.csr
+    openssl ca  -batch -passin pass:"whatever" -config ca.cnf -in client_ocsp.csr -out revoked_client_ocsp.pem -keyfile ca.key -cert ca.pem
+
+    cat client_ocsp.key >> revoked_client_ocsp.pem
+
+    # generate ocsp
+    openssl req -new -nodes -out ocsp.csr -keyout ocsp.key -config ocsp.cnf
+    openssl ca -batch -passin pass:"whatever" -config ocsp.cnf -in ocsp.csr -out ocsp.pem -keyfile ca.key -cert ca.pem
+
+    cat ocsp.key >> ocsp.pem
+
+    # revoke certificate
+    openssl ca -batch -revoke revoked_client_ocsp.pem -keyfile ca.key -cert ca.pem -config ca.cnf -passin pass:"whatever"
   fi
 }
 
 main() {
+  append_ocsp_endpoints
   generate_certs
   generate_revoked_client_certs
   sleep infinity
--- test/scripts/ocsp_responder.sh
+++ test/scripts/ocsp_responder.sh
@@ -1,16 +1,4 @@
-#!/bin/bash
-set -euo pipefail
+#!/bin/sh
 
-prefix=/etc/freeradius/3.0
-cp -pr ./test/certs_config/ocsp.cnf $prefix/certs
-
-cd $prefix/certs
-openssl req -new -nodes -out ocsp.csr -keyout ocsp.key -config ocsp.cnf
-openssl ca -batch -in ocsp.csr -out ocsp.pem -keyfile ca.key -cert ca.pem -config ocsp.cnf -passin pass:"whatever"
-openssl ocsp -port 9999 -index index.txt -rsigner ocsp.pem -rkey ocsp.key -CA ca.pem -text -out /tmp/ocsp.log &
-
-# local verification of the certificates
-# openssl ocsp -CAfile ca.pem -issuer ca.pem -cert client.pem -url http://localhost:9999 -resp_text
-
-# revokes the certificates
-# openssl ca -revoke client.pem -keyfile ca.key -cert ca.pem -config ca.cnf -passin pass:"whatever"
+cd test/certs
+openssl ocsp -port 8080 -index index.txt -rsigner ocsp.pem -rkey ocsp.key -CA ca.pem -text -out /tmp/ocsp.log &
