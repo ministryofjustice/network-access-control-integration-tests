9a56ab5188b7ec71e383a31dd1ab80c47e348435
Yusuf <yusuf@madetech.com>
2021-12-02 15:44:56 +0000

    Add integration schema test for fallback policy
    Co-authored-by: Csaba <csaba@madetech.com>

--- policy_engine_schema_test_client/test/test_schema.py
+++ policy_engine_schema_test_client/test/test_schema.py
@@ -8,8 +8,8 @@ from datetime import date
 from pymysql import connect, cursors
 
 class test_schema(unittest.TestCase):
-  def __init__(self):
-    self.conn = connect(
+  def connectDb(self):
+    return connect(
       host=os.environ.get('DB_HOST'),
       user=os.environ.get('DB_USER'),
       password=os.environ.get('DB_PASS'),
@@ -17,40 +17,37 @@ class test_schema(unittest.TestCase):
       cursorclass=cursors.DictCursor
     )
 
+
   def setUp(self):
-    
-    cursor = self.conn.cursor()
+    conn = self.connectDb()
+
+    cursor = conn.cursor()
     cursor.execute("insert into sites (id, name, tag) values (%s, %s,  %s);",(1, 'Site1','site_1'))
-    cursor.execute("insert into policies (id, name, description, fallback) values (%s, %s, %s, %s);",(1,'Policy_1','whatever', False))
-    cursor.execute("insert into rules (request_attribute, operator, value, policy_id) values (%s, %s, %s, %s);",('Tunnel-Type','equals', "Vlan", 1))
-    cursor.execute("insert into responses (response_attribute, value, policy_id) values (%s, %s, %s);",('Reply-Message','Hello', 1))
-    cursor.execute("insert into site_policies (site_id, policy_id, priority) values (%s, %s, %s);",(1, 1, 1))
+    cursor.execute("insert into policies (id, name, description, fallback) values (%s, %s, %s, %s);",(1,'Fallback Policy','whatever', True))
+    cursor.execute("insert into policies (id, name, description, fallback) values (%s, %s, %s, %s);",(2,'Non-fallback Policy','whatever', False))
+    cursor.execute("insert into responses (response_attribute, value, policy_id) values (%s, %s, %s);",('Reply-Message','Hello, this is fallback', 1))
+    cursor.execute("insert into responses (response_attribute, value, policy_id) values (%s, %s, %s);",('Reply-Message','Bye, this is not fallback', 2))
+    cursor.execute("insert into rules (request_attribute, operator, value, policy_id) values (%s, %s, %s, %s);",('Tunnel-Type','equals', "Vlan", 2))
+    cursor.execute("insert into site_policies (site_id, policy_id, priority) values (%s, %s, %s);",(1, 1, None))
+    cursor.execute("insert into site_policies (site_id, policy_id, priority) values (%s, %s, %s);",(1, 2, 1))
+    conn.commit()
 
   def tearDown(self):
+    conn = self.connectDb()
 
-    cursor = self.conn.sursor()
-    cursor.execute_many(["delete from rules;", "delete from responses;", "delete from site_policies;", "delete from policies;", "delete from sites;"])
-    self.conn.commit()
+    cursor = conn.cursor()
+
+    for table in ['rules', 'responses', 'site_policies', 'policies', 'sites']:
+      cursor.execute(f"DELETE FROM `{table}`")
+
+    conn.commit()
 
 
-
-
-
-    #  t.string "operator", null: false
-    # t.text "value", null: false
-    # t.bigint "policy_id", null: false
-    # t.string "request_attribute", null: false
-    # t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    # t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    # t.index ["policy_id"], name: "index_rules_on_policy_id"
-    
-    
-    connection.commit()
-
-  def test_(self):
+  def test_fallback_policy(self):
     payload = {'EAP-Type': 'TLS', 'Client-Shortname': 'site_1'}
     result = policy_engine.post_auth(payload)
-    self.assertEqual(result, ('OK', {'reply': ()}))
+    self.assertEqual(result, ('OK', {'reply': (('Reply-Message', 'Hello, this is fallback'),)}))
+
 
 if __name__ == '__main__':
     unittest.main()
